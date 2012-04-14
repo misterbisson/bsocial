3e4116759901ce53bf90e7d1eb5c6023cefa266a
Casey Bisson <casey.bisson@gmail.com>
2012-04-14 23:31:23 +0000

    turned it into a class to make setting the api keys easier
    git-svn-id: http://plugins.svn.wordpress.org/bsocial/trunk@531210 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/fb-api.php
+++ components/fb-api.php
@@ -1,64 +1,66 @@
 <?php
-/*
-Plugin Name: Facebook JS API
-Plugin URI: 
-Description: Includes the Facebook JS API,
-Author: Casey Bisson
-Author URI: http://MaisonBisson.com/
-Version: 1.1
-Text Domain: opengraph
-*/
 
-define('FBJS_NS_URI', 'http://www.facebook.com/2008/fbml');
-$fbjs_ns_set = false;
-
-
-function fbjs_opengraph_metadata( $properties )
+class bSocial_FacebookApi
 {
-	$properties['fb:admins'] = FBJS_ADMINS;
-	$properties['fb:app_id'] = FBJS_APP_ID; 
 
-	return $properties;
-}
-add_filter( 'opengraph_metadata' , 'fbjs_opengraph_metadata' );
+	function __construct()
+	{
+		$this->admins = FBJS_ADMINS;
+		$this->app_id = FBJS_APP_ID;
+		$this->namespace = 'http://www.facebook.com/2008/fbml';
 
-function fbjs_add_namespace( $output )
-{
-	global $fbjs_ns_set;
-	$fbjs_ns_set = true;
+		add_action( 'init' , 'init' );
+	}
 
-	$output .= ' xmlns:fb="' . esc_attr( FBJS_NS_URI ) . '"';
+	function init()
+	{
+		if( is_admin() )
+			return;
 
-	return $output;
-}
-add_filter( 'language_attributes' , 'fbjs_add_namespace' );
+		add_filter( 'opengraph_metadata' , 'opengraph_metadata' );
+		add_filter( 'language_attributes' , 'add_namespace' );
+		add_action( 'get_footer' , 'inject_js' );
+		add_filter( 'the_content' , 'inject_like_button' );
+	}
 
-function fbjs_include_js( $output )
-{
-	global $post;
+	function opengraph_metadata( $properties )
+	{
+		$properties['fb:admins'] = $this->admins;
+		$properties['fb:app_id'] = $this->app_id; 
+
+		return $properties;
+	}
+
+	function add_namespace( $output )
+	{
+		$output .= ' xmlns:fb="'. esc_attr( $this->namespace ) .'"';
+
+		return $output;
+	}
+
+	function inject_js( $output )
+	{
+		global $post;
 ?>
-	<div id="fb-root"></div>
-	<script>
-		window.fbAsyncInit = function() {
-			FB.init({appId: <?php echo FBJS_APP_ID; ?>, status: true, cookie: true, xfbml: true});
-		};
+		<div id="fb-root"></div>
+		<script>
+			window.fbAsyncInit = function() {
+				FB.init({appId: <?php echo FBJS_APP_ID; ?>, status: true, cookie: true, xfbml: true});
+			};
 
-		var e = document.createElement('script');
-		e.type = 'text/javascript';
-		e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-		e.async = true;
-		document.getElementById('fb-root').appendChild(e);
-	</script>
+			var e = document.createElement('script');
+			e.type = 'text/javascript';
+			e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+			e.async = true;
+			document.getElementById('fb-root').appendChild(e);
+		</script>
 <?php
+	}
+
+	function inject_like_button( $content )
+	{
+		$button = '<p><fb:like href="'. get_permalink( get_the_ID() ) .'"></fb:like></p>';
+
+		return $button . $content . $button;
+	}
 }
-add_action( 'get_footer' , 'fbjs_include_js' );
-
-function fbjs_add_like_button( $content )
-{
-	global $post;
-
-	$button = '<p><fb:like href="'. get_permalink( $post->ID ) .'"></fb:like></p>';
-
-	return $button . $content . $button;
-}
-add_filter( 'the_content' , 'fbjs_add_like_button' );
