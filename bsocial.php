1a31c51ea04082b6d5dfd7d36546cfbce90b4b7b
Will Luo <will.luo@gigaom.com>
2014-01-13 11:32:50 -0800

    incremented major version number since we're making a pretty sizable change to bsocial

--- bsocial.php
+++ bsocial.php
@@ -3,7 +3,7 @@
 Plugin Name: bSocial Connected Blogging Tools
 Plugin URI: http://wordpress.org/plugins/bsocial/
 Description: Social widgets and connectivity.
-Version: 5.2
+Version: 6.0
 Author: Casey Bisson
 Author URI: http://maisonbisson.com/blog/
 */
