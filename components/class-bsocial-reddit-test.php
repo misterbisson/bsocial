0d360aee0b650c2bd552d5c794d0ea16f3b2ecf1
Will Luo <will.luo@gigaom.com>
2014-01-16 15:00:35 -0800

    streamlined the arguments for bSocial_Reddit::get_links_by_domain()
    implemented bSocial_Reddit::next() to retrieve the next page of results.

--- components/class-bsocial-reddit-test.php
+++ components/class-bsocial-reddit-test.php
@@ -16,9 +16,38 @@ class bSocialReddit_Test
 		}
 
 		?>
-		<h2>Getting links to <a href="http://gigaom.com">gigaom.com</a></h2>
-		<pre>print_r( bsocial()->reddit()->get_links_by_domain( 'gigaom.com' ) );</pre>
-		<pre><?php print_r( bsocial()->reddit()->get_links_by_domain( 'gigaom.com', 3 ) ); ?></pre>
+		<h2>Getting 3 top submissions of <a href="http://gigaom.com">gigaom.com</a> links</h2>
+		<pre>print_r( bsocial()->reddit()->get_links_by_domain( array( 'domain' => 'gigaom.com', 'type' => 'top', 'limit' => 3 ) ) );</pre>
+		<pre><?php print_r( bsocial()->reddit()->get_links_by_domain( array( 'domain' => 'gigaom.com', 'type' => 'top', 'limit' => 3 ) ) ); ?></pre>
+
+		<h2>Getting 3 new submissions of <a href="http://gigaom.com">gigaom.com</a> links with error</h2>
+		<pre>print_r( bsocial()->reddit()->get_links_by_domain( array( 'domain' => 'gigaom.com', 'type' => 'newincorrecttype', 'limit' => 3 ) ) );</pre>
+		<pre><?php $ret = bsocial()->reddit()->get_links_by_domain( array( 'domain' => 'gigaom.com', 'type' => 'newincorrecttype', 'limit' => 3 ) );
+				print_r( bsocial()->reddit()->errors ); ?></pre>
+
+		<h2>Getting 2 new submissions of <a href="http://pro.gigaom.com">pro.gigaom.com</a> links</h2>
+		<pre>print_r( bsocial()->reddit()->get_links_by_domain( array( 'domain' => 'pro.gigaom.com', 'type' => 'new', 'limit' => 2 ) ) );</pre>
+		<pre><?php print_r( bsocial()->reddit()->get_links_by_domain( array( 'domain' => 'pro.gigaom.com', 'type' => 'new', 'limit' => 2 ) ) ); ?></pre>
+
+		<h2>Getting page 5 (with page size of 2) of top submissions of <a href="http://gigaom.com">gigaom.com</a> links</h2>
+		<pre>
+$res = bsocial()->reddit()->get_links_by_domain( array( 'domain' => 'gigaom.com', 'type' => 'top', 'limit' => 2 ); // page 1
+
+// skip the next 3 pages
+for ( $i = 0; $i < 3; ++$i )
+{
+	$res = bsocial()->reddit()->next();
+}
+print_r( bsocial()->reddit()->next() );</pre>
+
+		<pre><?php
+			$res = bsocial()->reddit()->get_links_by_domain( array( 'domain' => 'gigaom.com', 'type' => 'top', 'limit' => 2 ) ); // page 1
+			// skip the next 3 pages
+			for ( $i = 0; $i < 3; ++$i )
+			{
+				$res = bsocial()->reddit()->next();
+			}
+			print_r( bsocial()->reddit()->next() ); ?></pre>
 
 		<?php
 		die;
