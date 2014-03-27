c208423092fab674fb04ec26a87c74a504d128ec
Jamie Poitra <jamie@gigaom.com>
2014-03-27 21:25:30 +0000

    Added company methods to linkedin stuff.

--- components/class-bsocial-linkedin.php
+++ components/class-bsocial-linkedin.php
@@ -30,6 +30,20 @@ class bSocial_LinkedIn
 		return $this->oauth;
 	}//END oauth
 
+	// prepend the LinkedIn api url if $query_url is not absolute
+	public function validate_query_url( $query_url, $parameters )
+	{
+		if (
+			0 !== strpos( $query_url, 'http://' ) &&
+			0 !== strpos( $query_url, 'https://' )
+		)
+		{
+			$query_url = 'http://api.linkedin.com/v1/' . $query_url;
+		}//END if
+
+		return $query_url;
+	}//END validate_query_url
+
 	/**
 	 * @param $id user's profile url or id #
 	 * @param $by 'url', 'token' or 'self'. linkedIn uses "member id" and
@@ -49,15 +63,15 @@ class bSocial_LinkedIn
 		switch( $by )
 		{
 			case 'url':
-				$url = $this->base_url . 'url=' . urlencode( $id );
+				$url = 'people/url=' . urlencode( $id );
 				break;
 
 			case 'token':
-				$url = $this->base_url . 'id=' . $id;
+				$url = 'people/id=' . $id;
 				break;
 
 			case 'self':
-				$url = $this->base_url . '~';
+				$url = 'people/~';
 				break;
 
 			default:
@@ -70,6 +84,8 @@ class bSocial_LinkedIn
 		}
 
 		// check the cache for the user info
+		$url = $this->validate_query_url( $url );
+
 		if ( ! $user = wp_cache_get( $id, 'linkedin_' . $by ) )
 		{
 			$user = $this->oauth()->get_http( $url );
@@ -104,16 +120,16 @@ class bSocial_LinkedIn
 
 		switch ( $by )
 		{
-			case 'token':
-				$url = $this->base_url . 'id=' . $id;
+			case 'url':
+				$url = 'people/url=' . urlencode( $id );
 				break;
 
-			case 'url':
-				$url = $this->base_url . 'url=' . urlencode( $id );
+			case 'token':
+				$url = 'people/id=' . $id;
 				break;
 
 			case 'self':
-				$url = $this->base_url . '~';
+				$url = 'people/~';
 				break;
 
 			default:
@@ -127,12 +143,11 @@ class bSocial_LinkedIn
 			'scope' => 'self',
 		);
 
-		return $this->oauth()->get_http( $url, $params );
+		return $this->oauth()->get_http( $this->validate_query_url( $url ), $params );
 	}//END get_updates
 
 	/**
-	 * share something to a user's linkedin feed. this can take a minute
-	 * to show up after a successful call.
+	 * Share something to a user's linkedin feed. this can take a minute to show up after a successful call.
 	 *
 	 * @param $parameters array of possible parameters from linkedin's
 	 *  share api:
@@ -151,47 +166,150 @@ class bSocial_LinkedIn
 			$this->oauth()->set_keyring_user_token( $user_id );
 		} // END if
 
-		$url = $this->base_url . '~/shares';
+		$url = 'people/~/shares';
 
-		// convert contents of $parameters into an object that can be
-		// converted to json later
+		// Convert contents of $parameters into an object that can be converted to JSON later
 		$json = new stdClass;
+
 		if ( isset( $parameters['comment'] ) )
 		{
 			$json->comment = $parameters['comment'];
-		}
+		} // END if
 
 		$json->content = new stdClass;
-		if ( isset( $parameters['title'] ) )
+
+		$content_fields = array(
+			'title',
+			'description',
+			'submitted-url',
+			'submitted-image-url',
+		);
+
+		foreach ( $content_fields as $field )
 		{
-			$json->content->title = $parameters['title'];
-		}
-		if ( isset( $parameters['description'] ) )
-		{
-			$json->content->description = $parameters['description'];
-		}
-		if ( isset( $parameters['submitted-url'] ) )
-		{
-			$json->content->{'submitted-url'} = $parameters['submitted-url'];
-		}
-		if ( isset( $parameters['submitted-image-url'] ) )
-		{
-			$json->content->{'submitted-image-url'} = $parameters['submitted-image-url'];
-		}
+			if ( isset( $parameters[ $field ] ) )
+			{
+				$json->content->$field = $parameters[ $field ];
+			} // END if
+		} // END foreach
 
 		if ( isset( $parameters['visibility'] ) )
 		{
 			$json->visibility = new stdClass;
 			$json->visibility->code = $parameters['visibility'];
-		}
+		} // END if
 
-		return $this->oauth()->post_http( $url, array(
-			'body' => json_encode( $json ),
-			'headers' => array(
-				'Content-Type' => 'application/json',
-				'x-li-format' => 'json',
-			),
-			'sign_parameters' => FALSE,
-		) );
-	}//END share
+		return $this->oauth()->post_http(
+			$this->validate_query_url( $url ),
+			array(
+				'body' => json_encode( $json ),
+				'headers' => array(
+					'Content-Type' => 'application/json',
+					'x-li-format' => 'json',
+				),
+				'sign_parameters' => FALSE,
+			)
+		);
+	} // END share
+
+	/**
+	 * Share something to a company's linkedin feed. this can take a minute to show up after a successful call.
+	 *
+	 * @param $parameters array of possible parameters from linkedin's
+	 *  share api:
+	 *  'comment': the status update text
+	 *  'title': title of the shared content (image, url, etc.)
+	 *  'description': description of the shared content
+	 *  'submitted-url': url of shared content
+	 *  'submitted-image-url': an image to go with the shared content url
+	 *  'visibility': who can see this comment ('connections-only' or 'anyone'
+	 * @param $company_id LinkedIn company id or universal-name of the company you want to share as
+	 * @param $user_id WP user_id of the user you want to act as
+	 */
+	public function company_share( $parameters, $company_id, $user_id = FALSE )
+	{
+		if ( $user_id )
+		{
+			$this->oauth()->set_keyring_user_token( $user_id, $user_id );
+		} // END if
+
+		// If company_id isn't numeric then it's a universal-name
+		if ( ! is_numeric( $company_id ) )
+		{
+			if ( ! $company_id = $this->get_company_id( $company_id ) )
+			{
+				return FALSE;
+			} // END if
+		} // END if
+
+		$url = 'companies/' . absint( $company_id ) . '/shares';
+
+		// Convert contents of $parameters into an object that can be converted to JSON later
+		$json = new stdClass;
+
+		if ( isset( $parameters['comment'] ) )
+		{
+			$json->comment = $parameters['comment'];
+		} // END if
+
+		$json->content = new stdClass;
+
+		$content_fields = array(
+			'title',
+			'description',
+			'submitted-url',
+			'submitted-image-url',
+		);
+
+		foreach ( $content_fields as $field )
+		{
+			if ( isset( $parameters[ $field ] ) )
+			{
+				$json->content->$field = $parameters[ $field ];
+			} // END if
+		} // END foreach
+
+		if ( isset( $parameters['visibility'] ) )
+		{
+			$json->visibility = new stdClass;
+			$json->visibility->code = $parameters['visibility'];
+		} // END if
+
+		return $this->oauth()->post_http(
+			$this->validate_query_url( $url ),
+			array(
+				'body' => json_encode( $json ),
+				'headers' => array(
+					'Content-Type' => 'application/json',
+					'x-li-format' => 'json',
+				),
+				'sign_parameters' => FALSE,
+			)
+		);
+	} // END company_share
+
+	/**
+	 * Get the ID of a company using it's universal name
+	 *
+	 * @param $universal_name LinkedIn universal-name of a company you want the ID of (i.e. practical-pickles)
+	 * @param $user_id WP user_id of the user you want to act as
+	 */
+	public function get_company_id( $universal_name, $user_id = FALSE )
+	{
+		if ( $user_id )
+		{
+			$this->oauth()->set_keyring_user_token( $user_id );
+		} // END if
+
+		$url = 'companies/universal-name=' . $universal_name;
+
+		$response = (array) $this->oauth()->get_http( $this->validate_query_url( $url ) );
+
+		if ( Keyring_Util::is_error( $response ) || ! isset( $response['id'] ) )
+		{
+			return FALSE;
+		} // END if
+
+		return $response['id'];
+	} // END get_company_id
 }//END class
